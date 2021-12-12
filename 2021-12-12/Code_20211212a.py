64d7a1442272148e3c41f95b721a5efa47310a5e
edallal <edallal@users.noreply.github.com>
2021-12-12 05:17:45 -0500

    Solved Dec 12 problems

---
+++ 2021-12-12/Code_20211212a.py
@@ -0,0 +1,55 @@
+def countPaths(adjList, cur, smallVisit):
+    numPaths = 0
+    for nxt in adjList[cur]:
+        if nxt == 'end':
+            numPaths += 1
+            continue
+        if nxt[0:1].islower():
+            if not smallVisit[nxt]:
+                smallVisit[nxt] = True
+                np = countPaths(adjList, nxt, smallVisit)
+                smallVisit[nxt] = False
+                numPaths += np
+        else:
+            np = countPaths(adjList, nxt, smallVisit)
+            numPaths += np
+    return numPaths
+
+def countPathsInit(adjList):
+    smallVisit = {}
+    for s in adjList:
+        if s == 'start' or s == 'end' or s[0:1].isupper():
+            continue
+        smallVisit[s] = False
+    return countPaths(adjList, 'start', smallVisit)
+
+def main():
+    adjList = {}
+    with open('Data.txt', 'r') as file:
+        for line in file:
+            s = line
+            if s[-1] == '\n':
+                s = s[:-1]
+            src_dest = s.split('-')
+            src = src_dest[0]
+            dest = src_dest[1]
+            if not src in adjList:
+                adjList[src] = []
+            if not dest in adjList:
+                adjList[dest] = []
+            if src == 'start' or dest == 'end':
+                adjList[src].append(dest)
+                continue
+            if src == 'end' or dest == 'start':
+                adjList[dest].append(src)
+                continue
+            if src[0:1].isupper() and dest[0:1].isupper():
+                continue
+            adjList[src].append(dest)
+            adjList[dest].append(src)
+    numPaths = countPathsInit(adjList)
+    print(numPaths)
+            
+if __name__ == "__main__":
+    main()
+    
