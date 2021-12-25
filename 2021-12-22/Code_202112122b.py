1184c07925ef3693bdde2ab2ed01de135c1b1685
edallal <edallal@users.noreply.github.com>
2021-12-25 11:57:51 -0500

    Completed Dec 22 problems

---
+++ 2021-12-22/Code_202112122b.py
@@ -0,0 +1,57 @@
+import re
+
+def intersect(c1, c2, f = 1):
+    xl = max(c1[0], c2[0])
+    xh = min(c1[1], c2[1])
+    yl = max(c1[2], c2[2])
+    yh = min(c1[3], c2[3])
+    zl = max(c1[4], c2[4])
+    zh = min(c1[5], c2[5])
+    if xl > xh or yl > yh or zl > zh:
+        return None
+    return (xl, xh, yl, yh, zl, zh, -c1[6]*f)
+
+def addCube(cubelist, cube):
+    applist = []
+    for c in cubelist:
+        ci = intersect(c, cube)
+        if ci is not None:
+            applist.append(ci)
+    if cube[6] > 0:
+        cubelist.append(cube)
+    for c in applist:
+        cubelist.append(c)
+
+
+def size(cube):
+    return cube[6]*(cube[1]-cube[0]+1)*(cube[3]-cube[2]+1)*(cube[5]-cube[4]+1)
+
+def countOn(cubelist):
+    lights = 0
+    for cube in cubelist:
+        lights += size(cube)
+    return lights
+
+def main():
+    inputlist = []
+    with open('Data.txt', 'r') as file:
+        for line in file:
+            toks = re.split(' x=|\.\.|,y=|,z=|\s', line)
+            if toks[0] == 'on':
+                inputlist.append((int(toks[1]), int(toks[2]),\
+                                 int(toks[3]), int(toks[4]),\
+                                 int(toks[5]), int(toks[6]), 1))
+            else:
+                inputlist.append((int(toks[1]), int(toks[2]),\
+                                 int(toks[3]), int(toks[4]),\
+                                 int(toks[5]), int(toks[6]), -1))
+    cubelist = []
+    for cube in inputlist:
+        addCube(cubelist, cube)
+        if len(cubelist) > 10000000:
+            break
+    print(len(cubelist))
+    print(countOn(cubelist))
+if __name__ == "__main__":
+    main()
+    
